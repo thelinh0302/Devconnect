7ee1202e55a07231a278078481d6fecf9cbb3a90
the linh <chaulinh0302cr7@gmail.com>
2020-05-13 15:19:14 +0700

    Commit 2

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
-
+//connect database
+connectDB();
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
