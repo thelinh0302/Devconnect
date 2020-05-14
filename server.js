995bc51e4b2964f3e577c92698033f3493ae655c
the linh <chaulinh0302cr7@gmail.com>
2020-05-14 10:00:44 +0700

    build api middleware login+create and update

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const connectDB = require('./config/db');
 const app = express();
 //connect database
 connectDB();
+
+//middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
